34e88e977a8007d90f951d3e67810dee866d0a87
sethzawadi-ui <seth.zawadi@student.moringaschool.com>
2025-09-03 20:12:56 +0300

    arithmetic lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
+const random = Math.floor(Math.random() * 100) + 1;
+
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+const max = Math.max(1, 5, 10, 15, 20);
+
+module.exports = {
+  num1,
+  num2,
+  multiply,
+  random,
+  num3,
+  num4,
+  mod,
+  max
+};
 // Write your code here
